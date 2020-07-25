aea3b7ae88d2b20bbe4970974201ece5631e6067
YRZILLA <xbaracusx@mail.ru>
2020-07-25 16:15:26 +0300

    Добавил header

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'yrzillawork@gmail.com';                 // Наш логин
-$mail->Password = '0z7q9p3mYORA';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('yrzillawork@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('', 'Pulse');   // От кого письмо 
 $mail->addAddress('libire3887@wwmails.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
